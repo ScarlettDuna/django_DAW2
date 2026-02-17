0cfe49ec2eefe98ccbae62ebc04308263d2b3d03
Arantxa Reinoso <154524564+ScarlettDuna@users.noreply.github.com>
2026-02-17 12:13:32 +0100

    sesión 2 completa

--- app/admin.py
+++ app/admin.py
@@ -1,2 +1,8 @@
 
 # Register your models here.
+from django.contrib import admin
+from .models import Proveedor, Topping, Pizza
+
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(Proveedor)
